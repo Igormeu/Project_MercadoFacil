5b81288acbd6de0ef871b73fb6f7fcfc274f2bdf
Igor Stenio <160430256+Igormeu@users.noreply.github.com>
2025-11-19 12:02:17 +0000

    retirando arquivos desnecessários

--- descopactar.py
+++
@@ -1,7 +0,0 @@
-import zipfile
-
-# Abre o arquivo ZIP no modo de leitura
-with zipfile.ZipFile('Versão 2 - Projeto.zip', 'r') as zip_ref:
-    # Extrai todos os arquivos para o diretório especificado
-    zip_ref.extractall('./')
-    print("Arquivo ZIP descompactado com sucesso!")   
