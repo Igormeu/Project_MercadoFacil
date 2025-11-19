e7ccc13677a1cd8255e6256f85092c37cd24c7db
Igor Stenio <160430256+Igormeu@users.noreply.github.com>
2025-11-19 12:00:22 +0000

    correcao

--- descopactar.py
+++ descopactar.py
@@ -3,5 +3,5 @@ import zipfile
 # Abre o arquivo ZIP no modo de leitura
 with zipfile.ZipFile('Versão 2 - Projeto.zip', 'r') as zip_ref:
     # Extrai todos os arquivos para o diretório especificado
-    zip_ref.extractall('diretorio_destino')
+    zip_ref.extractall('./')
     print("Arquivo ZIP descompactado com sucesso!")   
