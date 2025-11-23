ef8e39112f276709a60ed027960012ed530239f3
VitorVLima <vitor14_df@hotmail.com>
2025-11-23 20:41:14 -0300

    atualizacoes do layout-final

--- MyApplication/app/build.gradle.kts
+++ MyApplication/app/build.gradle.kts
@@ -45,4 +45,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("androidx.gridlayout:gridlayout:1.0.0")
+    implementation("com.google.android.material:material:1.12.0")
 }
--- MyApplication/app/src/main/java/com/example/myapplication/SessaoManager.java
+++ MyApplication/app/src/main/java/com/example/myapplication/SessaoManager.java
@@ -3,29 +3,30 @@ package com.example.myapplication;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+
 public class SessaoManager {
 
     private static final String PREF_NAME = "MyAppSessao";
 
-    // Chaves
     private static final String KEY_IS_LOGGED_IN = "isLoggedIn";
     private static final String KEY_USER_EMAIL = "userEmail";
+    private static final String KEY_USER_NAME = "userName";
 
     private SharedPreferences pref;
     private SharedPreferences.Editor editor;
     private Context _context;
 
     public SessaoManager(Context context) {
+
         this._context = context;
         pref = _context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
         editor = pref.edit();
     }
 
-
-    public void criarSessaoLogin(String email) {
-
+    public void criarSessaoLogin(String email, String nomeCompleto) {
         editor.putBoolean(KEY_IS_LOGGED_IN, true);
         editor.putString(KEY_USER_EMAIL, email);
+        editor.putString(KEY_USER_NAME, nomeCompleto);
         editor.commit();
     }
 
@@ -36,6 +37,11 @@ public class SessaoManager {
 
     public String getEmailUsuario() {
         return pref.getString(KEY_USER_EMAIL, null);
+    }
+
+    // NOVO MÉTODO
+    public String getNomeUsuario() {
+        return pref.getString(KEY_USER_NAME, null);
     }
 
     public void logoutUser() {
